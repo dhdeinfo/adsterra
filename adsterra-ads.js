98bbe3f4d78db085819e6bf714d7e3383708435c
Adiyan Mustahim <50477931+dhdeinfo@users.noreply.github.com>
2024-05-31 14:13:56 +0700

    Create adsterra-ads.js
    Awal-

---
+++ adsterra-ads.js
@@ -0,0 +1,16 @@
+// adsterra-ads.js
+
+(function() {
+    var atOptions = {
+        'key' : 'd3d5907df5a43dd10c223a284cc7835e',
+        'format' : 'iframe',
+        'height' : 250,
+        'width' : 300,
+        'params' : {}
+    };
+
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = '//www.topcreativeformat.com/d3d5907df5a43dd10c223a284cc7835e/invoke.js';
+    document.body.appendChild(script);
+})();
