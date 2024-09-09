13fe572f13c26da344535e7f09a9973955502e0a
Adiyan Mustahim <50477931+dhdeinfo@users.noreply.github.com>
2024-09-09 14:39:37 +0700

    Create movie.js

---
+++ movie.js
@@ -0,0 +1,9 @@
+// Kode iklan yang disimpan di GitHub
+atOptions = {
+    'key' : '1b150b8f59ea7710fa35cd557a9e5e01',
+    'format' : 'iframe',
+    'height' : 90,
+    'width' : 728,
+    'params' : {}
+};
+document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/1b150b8f59ea7710fa35cd557a9e5e01/invoke.js"></scr' + 'ipt>');
