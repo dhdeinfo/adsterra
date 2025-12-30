52b1c7865edbf9908854c9d813d479e3e52c1dc8
Adiyan Mustahim <50477931+dhdeinfo@users.noreply.github.com>
2025-12-30 14:43:26 +0700

    Create ads.js

---
+++ ads.js
@@ -0,0 +1,16 @@
+(function () {
+    // Config Adsterra
+    window.atOptions = {
+        'key' : 'a244eca21d18982c663edd792b11837c',
+        'format' : 'iframe',
+        'height' : 250,
+        'width' : 300,
+        'params' : {}
+    };
+
+    // Load script Adsterra
+    var s = document.createElement("script");
+    s.src = "https://www.highperformanceformat.com/a244eca21d18982c663edd792b11837c/invoke.js";
+    s.async = true;
+    document.body.appendChild(s);
+})();
